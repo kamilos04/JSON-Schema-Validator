44f9e43359dcc207e7cc057ae9180b0898f28836
Jakub Urbaniec <kubaurbaniec1@gmail.com>
2025-12-30 16:34:57 +0100

    added first version of numbers validator

--- backend/app/validators/numbers.py
+++ backend/app/validators/numbers.py
@@ -17,5 +17,43 @@ class NumberValidator(Validator):
         errors: List[Dict] = []
 
 
+        if not isinstance(data, (int, float)) or isinstance(data, bool):
+            errors.append({"message": "Data is not a number", "path": path, "line": line})
+            return {"valid": False, "errors": errors}
+
+        if "minimum" in schema and data < schema["minimum"]:
+            errors.append({
+                "message": f"Number is smaller ({data}) than minimum ({schema['minimum']})",
+                "path": path,
+                "line": line
+            })
+
+        if "maximum" in schema and data > schema["maximum"]:
+            errors.append({
+                "message": f"Number is bigger ({data}) than maximum ({schema['maximum']})",
+                "path": path,
+                "line": line
+            })
+
+        if "exclusiveMinimum" in schema and data <= schema["exclusiveMinimum"]:
+            errors.append({
+                "message": f"Number is smaller or equal ({data}) than exclusiveMinimum ({schema['exclusiveMinimum']})",
+                "path": path,
+                "line": line
+            })
+
+        if "exclusiveMaximum" in schema and data >= schema["exclusiveMaximum"]:
+            errors.append({
+                "message": f"Number is bigger or equal ({data}) than exclusiveMaximum ({schema['exclusiveMaximum']})",
+                "path": path,
+                "line": line
+            })
+
+        if "multipleOf" in schema and data % schema["multipleOf"] != 0:
+            errors.append({
+                "message": f"Number is not a multiple of ({schema['multipleOf']})",
+                "path": path,
+                "line": line
+            })
 
         return {"valid": not errors, "errors": errors}
