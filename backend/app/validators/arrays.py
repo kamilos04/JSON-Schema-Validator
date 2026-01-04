b7efaa38cc5c63e8e44b8a88dea83c3372cd8518
Jakub Urbaniec <kubaurbaniec1@gmail.com>
2026-01-04 22:08:57 +0100

    added first version of array validator (without items keyword) and ArrayValidator unit tests

--- backend/app/validators/arrays.py
+++ backend/app/validators/arrays.py
@@ -16,7 +16,25 @@ class ArrayValidator(Validator):
 
         errors: List[Dict] = []
 
+        if not isinstance(data, list):
+            errors.append({"message": "Data is not an array", "path": path, "line": line})
+            return {"valid": False, "errors": errors}
 
+        if "items" in schema:
+            pass
 
+        if "minItems" in schema and len(data) < schema["minItems"]:
+            errors.append({
+                "message": f"Array length ({len(data)}) is smaller than minItems ({schema['minItems']})",
+                "path": path,
+                "line": line
+            })
+
+        if "maxItems" in schema and len(data) > schema["maxItems"]:
+            errors.append({
+                "message": f"Array length ({len(data)}) is bigger than maxItems ({schema['maxItems']})",
+                "path": path,
+                "line": line
+            })
 
         return {"valid": not errors, "errors": errors}
---
+++ backend/tests/validators/arrays.py
@@ -0,0 +1,58 @@
+import unittest
+from backend.app.validators.arrays import ArrayValidator
+
+class TestArrayValidator(unittest.TestCase):
+    def setUp(self):
+        self.validator = ArrayValidator()
+        self.path = "root"
+        self.line = 1
+
+    def test_valid_array(self):
+        schema = {"minItems" : 1, "maxItems" : 3}
+        data = [1, "abc"]
+        result = self.validator.validate(data, schema, self.path, self.line)
+        self.assertTrue(result["valid"])
+        self.assertEqual(len(result["errors"]), 0)
+
+    def test_not_an_array(self):
+        schema = {"maxItems" : 3}
+        data = 10
+        result = self.validator.validate(data, schema, self.path, self.line)
+        self.assertFalse(result["valid"])
+        self.assertEqual(len(result["errors"]), 1)
+        self.assertIn("Data is not an array", result["errors"][0]["message"])
+
+    """
+    def test_items_violation(self):
+        schema = {}
+        data = []
+        result = self.validator.validate(data, schema, self.path, self.line)
+        self.assertFalse(result["valid"])
+        self.assertEqual(len(result["errors"]), 1)
+        self.assertIn("", result["errors"][0]["message"])
+    """
+    def test_minItems_violation(self):
+        schema = {"minItems" : 3}
+        data = [1,"abc"]
+        result = self.validator.validate(data, schema, self.path, self.line)
+        self.assertFalse(result["valid"])
+        self.assertEqual(len(result["errors"]), 1)
+        self.assertIn("Array length 2 is smaller than minItems 3", result["errors"][0]["message"])
+
+    def test_maxItems_violation(self):
+        schema = {"maxItems": 3}
+        data = [1, "abc", True, 2]
+        result = self.validator.validate(data, schema, self.path, self.line)
+        self.assertFalse(result["valid"])
+        self.assertEqual(len(result["errors"]), 1)
+        self.assertIn("Array length 4 is bigger than maxItems 3", result["errors"][0]["message"])
+
+    def test_multiple_violations(self):
+        schema = {"minItems": 5, "maxItems": 3}
+        data = [1, "abc", True, 2]
+        result = self.validator.validate(data, schema, self.path, self.line)
+        self.assertFalse(result["valid"])
+        self.assertEqual(len(result["errors"]), 2)
+
+if __name__ == "__main__":
+    unittest.main()
