4dbe404a95fe542497e89bfd42ea60ac49d58118
Kamil Jach <kamiljach2004@gmail.com>
2026-01-22 21:52:24 +0100

    fix/dependency lazy loading in dispatcher

--- backend/app/validators/dispatcher.py
+++ backend/app/validators/dispatcher.py
@@ -1,15 +1,18 @@
-from typing import Any, Callable, List, Optional, Tuple
+from typing import Any, Callable, List, Optional, Tuple, Union
 from backend.app.validators.base import Validator
 
 class ValidatorDispatcher:
     def __init__(self):
-        self._strategies: List[Tuple[Callable[[Any], bool], Validator]] = []
+        self._strategies: List[Tuple[Callable[[Any], bool], Union[Validator, Callable[[], Validator]]]] = []
 
-    def register(self, condition: Callable[[Any], bool], validator: Validator):
+    def register(self, condition: Callable[[Any], bool], validator: Union[Validator, Callable[[], Validator]]):
         self._strategies.append((condition, validator))
 
     def get_validator(self, data: Any) -> Optional[Validator]:
         for condition, validator in self._strategies:
             if condition(data):
-                return validator
+                if isinstance(validator, Validator):
+                    return validator
+                elif callable(validator):
+                    return validator()
         return None
